b04d02ce7f5f3787badb6fed3f35a9ccff730ddd
Jeremy Smith <Jeremy_smith3@apple.com>
2018-11-29 11:47:20 -0800

    first commit

---
+++ src/Details.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Details extends React.Component {
+
+	render() {
+		return (
+
+			<div>
+				something is here
+			</div>
+
+		)
+	}
+}
+
+export default Details;
